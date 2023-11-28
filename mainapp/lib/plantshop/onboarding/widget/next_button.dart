ec87527cedc85f195b5066f45ad3a1c30a1e653e
Toan Nguyen <toan.nguyen@zuhlke.com>
2023-11-28 15:08:20 +0700

    [Erifaz-16] Plant shop add onboarding screen

--- mainapp/lib/plantshop/onboarding/widget/next_button.dart
+++ mainapp/lib/plantshop/onboarding/widget/next_button.dart
@@ -21,7 +21,130 @@ class OnboardingNextButton extends StatelessWidget {
       child: AnimatedBuilder(
         animation: animationController,
         builder: (context, child) {
-          return buildButton(context);
+          if (current == 2) {
+            double rotateValue = Tween<double>(begin: 0, end: 180)
+                .animate(CurvedAnimation(
+                  curve: Curves.easeOut,
+                  parent: animationController,
+                ))
+                .value;
+            double scaleValue = Tween<double>(begin: 1.0, end: 0.5)
+                .animate(CurvedAnimation(
+                  curve: Curves.easeOut,
+                  parent: animationController,
+                ))
+                .value;
+            double slideValue = Tween<double>(begin: 0, end: -120)
+                .animate(
+                  CurvedAnimation(
+                    curve: Curves.easeOut,
+                    parent: animationController,
+                  ),
+                )
+                .value;
+            return Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                Transform.translate(
+                  offset: Offset(slideValue, 0),
+                  child: Transform.rotate(
+                    angle: rotateValue * (3.14159265359 / 180),
+                    child: Transform.scale(
+                      scale: scaleValue,
+                      child: child,
+                    ),
+                  ),
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const SizedBox(height: 30),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 5,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(
+                          width: 1,
+                          color: AppColor.primary,
+                        ),
+                      ),
+                      child: TextButton(
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {
+                          // Navigator.push(
+                          //   context,
+                          //   CupertinoPageRoute<Widget>(
+                          //     builder: (BuildContext context) => NamePage(
+                          //       key: key,
+                          //       carouselController: carouselController,
+                          //     ),
+                          //   ),
+                          // );
+                        },
+                        child: const Text(
+                          'Signup',
+                          style: TextStyle(
+                            color: AppColor.primary,
+                            fontSize: 28,
+                          ),
+                        ),
+                      ),
+                    ),
+                    // sh(15),
+                    // FadeInUp(
+                    //   child: RichText(
+                    //     textAlign: TextAlign.center,
+                    //     text: TextSpan(
+                    //       children: <TextSpan>[
+                    //         TextSpan(
+                    //           text: l(context).alreadyAccount,
+                    //           style: const TextStyle(
+                    //             color: Color.fromARGB(255, 96, 96, 96),
+                    //             fontSize: 16,
+                    //             fontWeight: FontWeight.w600,
+                    //           ),
+                    //         ),
+                    //         TextSpan(
+                    //           text: l(context).login,
+                    //           style: const TextStyle(
+                    //             color: Colors.blue,
+                    //             decoration: TextDecoration.underline,
+                    //             fontWeight: FontWeight.w400,
+                    //           ),
+                    //           recognizer: TapGestureRecognizer()
+                    //             ..onTap = () async {
+                    //               Navigator.push(
+                    //                 context,
+                    //                 CupertinoPageRoute<Widget>(
+                    //                   builder: (BuildContext context) =>
+                    //                       SignInPage(
+                    //                     key: key,
+                    //                     carouselController: carouselController,
+                    //                   ),
+                    //                 ),
+                    //               );
+                    //             },
+                    //         ),
+                    //       ],
+                    //     ),
+                    //   ),
+                    // ),
+                  ],
+                ),
+              ],
+            );
+          } else {
+            return buildButton(context);
+          }
         },
         child: buildButton(context),
       ),
